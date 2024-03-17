0520370626f7fa3efa4fd5f325c16dff91260e2f
hukuryo <ryohei.1998.05.09@gmail.com>
2024-03-17 21:41:06 +0900

    Vitestを勉強

--- src/utils/sum.test.ts
+++ src/utils/sum.test.ts
@@ -1,7 +1,20 @@
-// sum.test.js
-import { expect, test } from "vitest";
+import { assert, expect, test } from "vitest";
 import { sum } from "./sum";
 
-test("adds 1 + 2 to equal 3", () => {
+test("sumメソッドの返り値が正しいかどうか", () => {
   expect(sum(1, 2)).toBe(3);
+});
+
+test("decimals are rounded to 5 after the point", () => {
+  expect(0.2 + 0.1).toBeCloseTo(0.3, 5);
+  expect(0.2 + 0.1).not.toBeCloseTo(0.3, 50);
+});
+
+test("assert", () => {
+  assert("foo" !== "bar", "foo should not be equal to bar");
+});
+
+test("assert fail demo", () => {
+  assert.fail("error message on failure");
+  assert.fail("foo", "bar", "foo is not bar", "===");
 });
